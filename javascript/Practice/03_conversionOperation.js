faacd8997a4d9aa042c399e871bc0eb9b84550d9
Abhishek Singh <singh.abhishek.2017447@gmail.com>
2024-05-26 02:21:32 +0530

    file upload

--- javascript/Practice/03_conversionOperation.js
+++ javascript/Practice/03_conversionOperation.js
@@ -4,14 +4,14 @@
 //let score = true
 let score = "abhi"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let ValueInNumber = Number(score)
 
-console.log(typeof ValueInNumber);
+//console.log(typeof ValueInNumber);
 
-console.log(ValueInNumber);
+//console.log(ValueInNumber);
 
 //"33" => 33
 // "33abc" => NaN
@@ -20,7 +20,7 @@ console.log(ValueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -29,5 +29,34 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ************* Operations ***********
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " abhi"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+//num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
